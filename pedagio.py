4989f9c03dbb5444feac9d0f714305cc64ddb31a
Felipe <felipenoll04@gmail.com>
2025-04-07 12:44:18 -0300

    Agora os dados são inseridos em um arquivo .csv, ao invés de diretamente no banco

--- pedagio.py
+++ pedagio.py
@@ -1,4 +1,5 @@
 import random
+import pandas as pd
 import psycopg2
 from faker import Faker
 
@@ -15,33 +16,37 @@ cursor = conn.cursor()
 fake = Faker()
 
 # Selecionar os ids das tabelas
-cursor.execute("DELETE FROM ticket")
 cursor.execute("SELECT id_pista FROM pista")
-pistas = cursor.fetchall()
+pistas = [row[0] for row in cursor.fetchall()]
 cursor.execute("SELECT id_praca_pedagio FROM praca_pedagio")
-pedagios = cursor.fetchall()
+pedagios = [row[0] for row in cursor.fetchall()]
 cursor.execute("SELECT id_funcionario FROM funcionario")
-funcionarios = cursor.fetchall()
+funcionarios = [row[0] for row in cursor.fetchall()]
 cursor.execute("SELECT id_veiculo FROM veiculo")
-veiculos = cursor.fetchall()
+veiculos = [row[0] for row in cursor.fetchall()]
 cursor.execute("SELECT id_valor_tipo_veiculo FROM valor_tipo_veiculo")
-valores_tipo_veiculo = cursor.fetchall()
+valores_tipo_veiculo = [row[0] for row in cursor.fetchall()]
 
-# Função para inserir tickets em lotes para não crashar
-def inserir_tickets(qtd):
-    batch_size = 10000
-    for i in range(1, qtd + 1, batch_size):
-        tickets = [
-            (j, fake.date_between(start_date="-1y"), random.choice(pistas)[0], random.choice(pedagios)[0],
-             random.choice(funcionarios)[0], random.choice(veiculos)[0], random.choice(valores_tipo_veiculo)[0])
-            for j in range(i, min(i + batch_size, qtd + 1))
-        ]
-        cursor.executemany("INSERT INTO ticket (id_ticket, data, id_pista, id_praca_pedagio, id_funcionario, id_veiculo, id_valor_tipo_veiculo) VALUES (%s, %s, %s, %s, %s, %s, %s)", tickets)
-        conn.commit()
-
-# Inserts dos tickets
-inserir_tickets(1000000)
-#inserir_tickets(10000000)
-#inserir_tickets(100000000)
 cursor.close()
 conn.close()
+
+# Função para gerar os tickets
+def gerar_tickets(qtd):
+    tickets = [{
+        "id_ticket": i,
+        "data": fake.date_between(start_date="-1y"),
+        "id_pista": random.choice(pistas),
+        "id_praca_pedagio": random.choice(pedagios),
+        "id_funcionario": random.choice(funcionarios),
+        "id_veiculo": random.choice(veiculos),
+        "id_valor_tipo_veiculo": random.choice(valores_tipo_veiculo)
+    } for i in range(1, qtd + 1)]
+
+    df = pd.DataFrame(tickets)
+    df.to_csv('tickets.csv', index=False)
+
+
+# Inserts dos tickets
+gerar_tickets(1000000)
+#gerar_tickets(10000000)
+#gerar_tickets(100000000)
